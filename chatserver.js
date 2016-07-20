a7cfae80cd4277ef477dc37d1526f5c4e5376dd0
vogtn <vogtn@uw.edu>
2016-07-20 15:40:03 -0700

    adding communication between multiple clients

--- chatserver.js
+++ chatserver.js
@@ -2,10 +2,18 @@ var net = require('net');
 
 var chatServer = net.createServer();
 
+var clientList = [];
+
 chatServer.on('connection', function(client){
   client.write('Hi!\n');
+
+  clientList.push(client);
+
   client.on('data',function(data){
-    console.log(data)
+    for(var i =0; i < clientList.length; i+=1){
+      //write data to all clientList
+      clientList[i].write(data);
+    }
   })
 
 })
