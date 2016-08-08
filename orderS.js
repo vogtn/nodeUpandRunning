6b3ac8626b3ddbbf9b19e5bf550bcde746f50c87
vogtn <vogtn@uw.edu>
2016-08-08 13:19:30 -0700

    declared functions

--- orderS.js
+++ orderS.js
@@ -13,3 +13,21 @@ server.on('request', getMemCached(req, res) {
     });
   });
 });
+
+
+// USING DECLARED FUNCTIONS
+var render = function(wsData) {
+  page = pageRender(req, session, userData, wsData);
+};
+
+var getWsInfo = function(userData) {
+  ws.get(req, render);
+};
+
+var getDbInfo = function(session) {
+  db.get(session.user, getWsInfo);
+};
+
+var getMemCached = function(req, res) {
+  memcached.getSession(req, getDbInfo);
+};
