896ec07d24c3a416e8dab85f5ecc15aac75ea589
vogtn <vogtn@uw.edu>
2016-08-20 13:32:33 -0700

    using multiple key hash at a time

---
+++ multiplehash.js
@@ -0,0 +1,18 @@
+var redis = require('redis'),
+  client = redis.createClient();
+
+client.on("error", function(err){
+  console.log("Error " + err);
+});
+
+console.log("Setting user hash");
+client.hmset("user", "username", "johndoe","firstname","john","lastname","doe");
+
+clienthkeys("user", function(err,replies){
+  console.log("Results for user:");
+  console.log(replies.length + " replies:");
+  replies.forEach(function(reply, i){
+    console.log(i + ": "+reply);
+  });
+  client.end();
+});
