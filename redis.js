6e8c3a31499ae8e97d361add82f7fb2c2ed63dcf
vogtn <vogtn@uw.edu>
2016-08-19 22:38:30 -0700

    basic redis get and set

---
+++ redis.js
@@ -0,0 +1,16 @@
+//npm install redis
+var redis = require('redis'),
+  client = redis.createClient();
+
+client.on("error", function(err){
+  console.log("Error " + err);
+});
+
+console.log("Setting key1");
+client.set("key1", "My string!", redis.print);
+console.log("Getting key1");
+client.get("key1", function (err, reply){
+  console.log("Results for key1:");
+  console.log(reply);
+  client.end();
+});
